509d896f4042087774e30d771f0aebaf63ee1e9d
Ben Shanks <bshanks@unc.edu>
2014-09-26 13:00:59 +0200

    Added allowed Decay, made a nice plot

--- fitAr39.cxx
+++ fitAr39.cxx
@@ -1,3 +1,7 @@
+//Pulls in normalized spectrum from Ar39.dat, generates unnormalized hist,
+//and fits forbidden & normal beta spectrum to it.
+//B. Shanks, 9/24/14
+
 #include "Riostream.h"
 #include <TROOT.h>
 #include <TStyle.h>
@@ -11,6 +15,7 @@ void fitAr39(){
     gStyle->SetCanvasBorderMode(0);
     gStyle->SetOptTitle(0);
     gStyle->SetOptStat(0);
+    gStyle->SetOptStat(0);
     
     
     TCanvas* c1 = new TCanvas();
@@ -18,18 +23,48 @@ void fitAr39(){
     //pull the data file into an artificial spectrum...
     TH1F *h1 = new TH1F("h1","^{39}Ar Spectrum",101,0,0.54);
     ReadArSpectrum(h1);
-    h1->SetStats(0);
-    h1->Draw();
-    c1->Update();
+//    h1->SetStats(0);
     
-    //now fit it...
+    //now fit it with forbidden and non-forbidden...
     
-    TF1* fBeta = new TF1("fBeta", "[0]*ForbiddenBetaSpec(x, 19, 39, 0.565)", 0, 0.565);
+    TF1* fBeta = new TF1("fBeta", "[0]*BetaSpec(x, 19, 39, 0.565)", 0, 0.565);
     fBeta->SetParameter(0,50000);
     fBeta->Draw();
-    h1->Fit("fBeta", "r");
+    h1->Fit("fBeta", "0nr");
+    c1->Update();
+    
+    TF1* fBetaForb = new TF1("fBetaForb", "[0]*ForbiddenBetaSpec(x, 19, 39, 0.565)", 0, 0.565);
+    fBetaForb->SetParameter(0,50000);
+    fBetaForb->SetLineColor(kMagenta);
+    fBetaForb->Draw("SAME");
+    h1->Fit("fBetaForb", "0nr");
     
     c1->Update();
+    
+    h1->Draw("Same");
+    c1->Update();
+    
+    double histMean = h1->GetMean();
+    double betaMean = fBeta->Mean(0,0.565);
+    double betaForbMean = fBetaForb->Mean(0,0.565);
+    
+    char legHist[500];
+    char legBeta[500];
+    char legBetaMean[500];
+    sprintf(legHist,".Dat File (Mean %.4f)", histMean);
+    sprintf(legBeta,"Allowed Beta (Mean %.4f)", betaMean);
+    sprintf(legBetaMean,"Forbidden Beta (Mean %.4f)", betaForbMean);
+    
+    TLegend* leg2 = new TLegend(.5, .75, .9, .9);
+    leg2->SetTextFont(72);
+    leg2->AddEntry(legHist,legHist,"l");
+    leg2->AddEntry(fBeta,legBeta,"l");
+    leg2->AddEntry(fBetaForb,legBetaMean,"l");
+    leg2->Draw();
+    
+    c1->Print("Argon_Beta_Spectra.pdf");
+    c1->Print("Argon_Beta_Spectra.root");
+    
 }
 
 
@@ -44,7 +79,7 @@ void ReadArSpectrum(TH1F* h1) {
     in.open("Ar39.dat");
     Float_t energy,spec;
     
-    int specMult = 10000; //whatever...
+    int specMult = 1000; //whatever...
     
     while (1) {
         in >> energy >> spec;
